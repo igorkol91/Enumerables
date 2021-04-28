1209e6dab99794248dc84722a85fdf6dc4009687
Victor Otieno <victorotieno598@gmail.com>
2021-04-28 13:52:09 +0300

    Fix enumerable spacing errors

--- enumerable.rb
+++ enumerable.rb
@@ -1,9 +1,9 @@
 module Enumerable
-  # rubocop:disable Metrics/CyclomaticComplexity
-  # rubocop:disable Metrics/PerceivedComplexity
-  # rubocop:disable Lint/ToEnumArguments
+# rubocop:disable Metrics/CyclomaticComplexity
+# rubocop:disable Metrics/PerceivedComplexity
+# rubocop:disable Lint/ToEnumArguments
 
-  ## method to check incoming data type
+## method to check incoming data type
   def check_data_type(data_type)
     arr = data_type if data_type.instance_of?(Array)
     arr = to_a if data_type.instance_of?(Range)
