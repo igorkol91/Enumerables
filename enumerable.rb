e62d6d70fd49cd5840fabc2513a0c6c45a6b712e
Igor.Koloski <koloski_igor@hotmail.com>
2021-04-29 11:15:31 +0200

    Fixing linter variable errors

--- enumerable.rb
+++ enumerable.rb
@@ -97,11 +97,9 @@ module Enumerable
   def my_count(arg = nil)
     counter = 0
     if arg.nil? && !block_given?
-      # rubocop:disable UselessAssignment
       for i in self
         counter += 1
       end
-      # rubocop:enable UselessAssignment
     elsif !arg.nil? and !block_given?
       for i in self
         counter += 1 if i == arg
